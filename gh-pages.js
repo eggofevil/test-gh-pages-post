ddaef4d40a3b40c93d909492b92b901c73fa05e0
A.Gurin <eoew@yandex.ru>
2023-11-23 16:43:43 +0300

    Updates

--- gh-pages.js
+++ gh-pages.js
@@ -10,7 +10,7 @@ const buildConfig = {
 const srcConfig = {
   // src: ["src/*", "public/*"],
   // src: "*!(node_modules)/*",
-  src: "**!(node_modules)",
+  src: "**!(node_modules)**",
   remote: "remote",
   branch: "master",
 };
