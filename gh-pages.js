a732cf0e557ae5c2a253d6964a118e92cde06d03
A.Gurin <eoew@yandex.ru>
2023-11-23 16:38:15 +0300

    Updates

---
+++ gh-pages.js
@@ -0,0 +1,30 @@
+const ghpages = require("gh-pages");
+const ghpagesCacheClean = require(".\\node_modules\\gh-pages\\bin\\gh-pages-clean.js");
+
+const buildFolder = "build";
+const projectFolder = ".";
+
+const buildConfig = {
+  remote: "remote",
+};
+const srcConfig = {
+  // src: ["src/*", "public/*"],
+  // src: "*!(node_modules)/*",
+  src: "**!(node_modules)",
+  remote: "remote",
+  branch: "master",
+};
+
+function deploy(dist, config) {
+  ghpages.publish(dist, config, (data) => {
+    console.log(data.error);
+    ghpagesCacheClean();
+  });
+}
+
+function startDeploy(args) {
+  //if (args.includes("-b")) deploy(buildFolder, buildConfig);
+  if (args.includes("-s")) deploy(projectFolder, srcConfig);
+}
+
+startDeploy(process.argv.slice(2));
