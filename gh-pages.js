0eb5fea33b8a3ed5da38835d82500a78a5a56e78
A.Gurin <eoew@yandex.ru>
2023-11-24 20:00:00 +0300

    Updates

--- gh-pages.js
+++ gh-pages.js
@@ -5,6 +5,9 @@
   .* - собирает все файлы начинающиеся с .
   *!(node_modules)/* - coбирает все папки кроме node-modules (кроме начинающихся с .)
 */
+
+console.log(process.argv);
+
 const ghpages = require("gh-pages");
 const ghpagesCacheClean = require(".\\node_modules\\gh-pages\\bin\\gh-pages-clean.js");
 
